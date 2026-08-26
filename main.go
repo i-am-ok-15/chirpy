5312b4af07166f2e5c1989d4d89e408aec7eed25
i-am-ok-15 <i-am-ok-15@github.com>
2026-08-26 09:41:31 +0100

    Lesson 1.4 complete

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	fmt.Println("Creating ServeMux to route requests...")
+	ServeMux := http.NewServeMux()
+
+	fmt.Println("Creating server struct...")
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        ServeMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	fmt.Println("Starting server...")
+	server.ListenAndServe()
+
 }
