125aa8b0e2cd1b0a6a8bd2b78d6de6796ca9f821
i-am-ok-15 <i-am-ok-15@github.com>
2026-08-26 12:28:35 +0100

    Lesson 1.9 complete

--- main.go
+++ main.go
@@ -6,24 +6,30 @@ import (
 	"time"
 )
 
-func main() {
+func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
+func main() {
 	log.Println("Creating ServeMux to route requests...")
-	ServeMux := http.NewServeMux()
+	mux := http.NewServeMux()
 
 	log.Println("Creating server struct...")
 	server := &http.Server{
 		Addr:           ":8080",
-		Handler:        ServeMux,
+		Handler:        mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	fileHandler := http.FileServer(http.Dir("."))
-	ServeMux.Handle("/", fileHandler)
+	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", fileHandler)
 
 	log.Println("Starting server...")
 	log.Fatal(server.ListenAndServe())
-
 }
