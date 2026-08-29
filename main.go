69cf7e72f80a1c6865876a2ff48395e4e8315375
i-am-ok-15 <i-am-ok-15@github.com>
2026-08-29 22:07:11 +0100

    lesson 4.2 complete

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -45,6 +46,73 @@ func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder((r.Body))
+	message := chirp{}
+	err := decoder.Decode(&message)
+	if err != nil {
+		log.Printf("Error decoding message: %s", err)
+		respBody := errorResponse{}
+		respBody.Error = "Something went wrong"
+		dat, _ := json.Marshal(respBody)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write(dat)
+		return
+	}
+
+	if len(message.Body) > 140 {
+		respBody := errorResponse{}
+		respBody.Error = "Chirp is too long"
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			respBody.Error = "Something went wrong"
+			dat, err = json.Marshal(respBody)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			w.Write(dat)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+
+	} else {
+		respBody := validResponse{}
+		respBody.Valid = true
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			respBody := errorResponse{}
+			respBody.Error = "Something went wrong"
+			dat, err = json.Marshal(respBody)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			w.Write(dat)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(dat)
+	}
+}
+
 func main() {
 	log.Println("Creating ServeMux to route requests...")
 	mux := http.NewServeMux()
@@ -63,8 +131,10 @@ func main() {
 	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerHitCount)
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileHandler))
