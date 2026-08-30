de9bf812cf0edcba3cd895fa375a93505df19034
i-am-ok-15 <i-am-ok-15@github.com>
2026-08-30 20:38:50 +0100

    lesson 5.5 complete

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 	"time"
+
+	"github.com/i-am-ok-15/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, _ := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+
 	log.Println("Creating ServeMux to route requests...")
 	mux := http.NewServeMux()
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		dbQueries: dbQueries,
+	}
 
 	log.Println("Creating server struct...")
 	server := &http.Server{
